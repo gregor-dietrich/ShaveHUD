e2ba246335173b273b1fc763753ccc2a60dd7fb2
Gregor Dietrich <11874515+gregor-dietrich@users.noreply.github.com>
2021-02-05 21:04:10 +0100

    merge CustomVehicleFOV into Scripts

--- mods/ShaveHUD/lua/Scripts.lua
+++ mods/ShaveHUD/lua/Scripts.lua
@@ -1447,4 +1447,26 @@ elseif string.lower(RequiredScript) == "lib/units/weapons/akimboweaponbase" then
 			time = 0.5
 		})
 	end
+elseif string.lower(RequiredScript) == "lib/tweak_data/vehicletweakdata" then
+	if not ShaveHUD:getSetting({"Misc", "VEHICLE_FOV"}, true) then
+		return
+	end
+
+	local old_init = VehicleTweakData.init
+	local new_fov = ShaveHUD:getSetting({"Misc", "VEHICLE_FOV_VALUE"}, 90)
+
+	function VehicleTweakData:init(tweak_data)
+		old_init(self, tweak_data)
+
+		self.falcogini.fov = new_fov
+		self.muscle.fov = new_fov
+		self.forklift.fov = new_fov
+		self.forklift_2.fov = new_fov
+		self.box_truck_1.fov = new_fov
+		self.mower_1.fov = new_fov
+		self.boat_rib_1.fov = new_fov
+		self.blackhawk_1.fov = new_fov
+		self.bike_1.fov = new_fov
+		self.bike_2.fov = new_fov
+	end
 end
