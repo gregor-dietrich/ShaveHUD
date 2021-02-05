6fbb8b844d54946300557f9da67494ecddb1f757
Gregor Dietrich <11874515+gregor-dietrich@users.noreply.github.com>
2021-02-05 20:59:23 +0100

    merge Akimbo Toggle into Scripts

--- mods/ShaveHUD/lua/Scripts.lua
+++ mods/ShaveHUD/lua/Scripts.lua
@@ -1434,4 +1434,17 @@ elseif string.lower(RequiredScript) == "lib/units/beings/player/states/playersta
             end
         end
     end
+elseif string.lower(RequiredScript) == "lib/units/weapons/akimboweaponbase" then
+	if not ShaveHUD:getSetting({"EQUIPMENT", "ENABLE_AKIMBO_MODE"}, true) then
+		return
+	end
+
+    function AkimboWeaponBase:toggle_akimbo_fire()
+		self._manual_fire_second_gun = not self._manual_fire_second_gun
+		local on = self._manual_fire_second_gun
+		managers.hud:show_hint({
+			text = on and "x1 fire" or "x2 fire",
+			time = 0.5
+		})
+	end
 end
