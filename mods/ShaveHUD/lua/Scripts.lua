cafd651ec235032782d6df2bd1d000b898f63461
Gregor Dietrich <11874515+gregor-dietrich@users.noreply.github.com>
2021-02-05 21:06:55 +0100

    merge ShieldDespawnTimer into Scripts

--- mods/ShaveHUD/lua/Scripts.lua
+++ mods/ShaveHUD/lua/Scripts.lua
@@ -1469,4 +1469,38 @@ elseif string.lower(RequiredScript) == "lib/tweak_data/vehicletweakdata" then
 		self.bike_1.fov = new_fov
 		self.bike_2.fov = new_fov
 	end
+elseif string.lower(RequiredScript) == "lib/units/enemies/cop/copinventory" then
+	if not ShaveHUD:getSetting({"Misc", "SHIELD_DESPAWN_TIMER"}, true) then
+		return
+	end
+
+	local this = {
+		min_value = 3,
+		max_value = 12
+	}
+
+	if not ShieldDespawnTimer then
+		ShieldDespawnTimer = ShieldDespawnTimer or {}
+
+		function ShieldDespawnTimer:get_time( skip_time )
+			local valid = type( self.timer ) == "number" and self.timer >= this.min_value and self.timer <= this.max_value
+			self.timer = valid and self.timer or this.min_value
+
+			return ( skip_time and 0 or Application:time() ) + self.timer
+		end
+		
+		Hooks:PostHook( CopInventory, "drop_shield", "ShieldDespawnTimer", function( u )
+			if alive( u._shield_unit ) then
+				local function clbk_hide()
+					if alive( u._shield_unit ) then
+						managers.enemy:unregister_shield( u._shield_unit )
+						u._shield_unit:set_slot( 0 )
+						u._shield_unit = nil
+					end
+				end
+
+				managers.enemy:add_delayed_clbk( "", clbk_hide, ShieldDespawnTimer:get_time() )
+			end
+		end)
+	end
 end
